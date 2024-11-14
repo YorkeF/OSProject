3de2e609117195f4f1d54b0d3a5cec9160fa2eec
coledunn <coledunn0226@gmail.com>
2024-11-13 18:17:29 -0600

    Update TransactionBlocks.cpp

--- TransactionBlocks.cpp
+++ TransactionBlocks.cpp
@@ -1,48 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include "TransactionBlocks.h"
 #include "Monitor.h"
 
-
-void deposit(Monitor *monitor, const char *accountId, double amount);
-void withdraw(Monitor *monitor, const char *accountId, double amount);
-void inquiry(Monitor *monitor, const char *accountId);
-void transfer(Monitor *monitor, const char *fromAccountId, double amount, const char *toAccountId);
-void closeAccount(Monitor *monitor, const char *accountId);
-void createAccount(Monitor *monitor, const char *accountId, const char *name, double initialBalance);
-
-void createAccount(Monitor *monitor, const char *accountId, const char *name, double initialBalance)
-{
+// Function to handle account creation
+void createAccount(Monitor *monitor, const char *accountId, const char *name, double initialBalance) {
     monitorCreateAccount(monitor, accountId, name, initialBalance);
-}
-
-void closeAccount(Monitor *monitor, const char *accountId)
-{
-    monitorCloseAccount(monitor, accountId);
-}
-
-void inquiry(Monitor *monitor, const char *accountId)
-{
-    monitorInquiry(monitor, accountId);
 }
 
 // Function to handle deposits
 void deposit(Monitor *monitor, const char *accountId, double amount) {
-   monitorDeposit(monitor, accountId, amount);
+    monitorDeposit(monitor, accountId, amount);
 }
-
-
 
 // Function to handle withdrawals
 void withdraw(Monitor *monitor, const char *accountId, double amount) {
     monitorWithdraw(monitor, accountId, amount);
 }
 
+// Function to handle balance inquiries
+void inquiry(Monitor *monitor, const char *accountId) {
+    monitorInquiry(monitor, accountId);
+}
 
-// Function to transfer funds between accounts
+// Function to handle transfers
 void transfer(Monitor *monitor, const char *fromAccountId, double amount, const char *toAccountId) {
     monitorTransfer(monitor, fromAccountId, amount, toAccountId);
 }
 
+// Function to handle account closure
+void closeAccount(Monitor *monitor, const char *accountId) {
+    monitorCloseAccount(monitor, accountId);
+}
 
